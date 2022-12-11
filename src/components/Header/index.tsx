81c73edc9fb72789de1f142bf108cb6e7b353c48
airtonm4 <airton.atmi@gmail.com>
2022-12-11 10:40:51 -0300

    Added navigate to open game add input on click

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
+import { useNavigate } from "react-router-dom"
 import headStyle from "./Header.module.css"
 
 export const Header = () => {
+
+  const navigate = useNavigate()
+
   return (
     <>
       <div className={headStyle.header} />
       <div className={headStyle.externa}>
-        <button className={headStyle.general_button}>Adicionar Jogos</button>
+        <button className={headStyle.general_button} onClick={() => { navigate("add-game") }}>Adicionar Jogos</button>
       </div>
     </>
   )
