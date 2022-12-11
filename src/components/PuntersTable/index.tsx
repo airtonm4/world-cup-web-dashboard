829be9c7507f406c91e908fee897c8fecc195f7e
airtonm4 <airton.atmi@gmail.com>
2022-12-11 17:40:06 -0300

    Added put request method

--- src/components/PuntersTable/index.tsx
+++ src/components/PuntersTable/index.tsx
@@ -1,8 +1,23 @@
+import { useState } from "react"
 import { PunterProps } from "../../contexts/PunterContext"
+import { usePuntersContext } from "../../contexts/PuntersContext"
+import api from "../../utils/api"
 import Games from "../Games/Games.module.css"
 import TableStyles from "../GamesTable/TableStyles.module.css"
 
 export const PuntersTable = ({ punters }: { punters: Array<PunterProps> }) => {
+
+  const { setPunters } = usePuntersContext()
+
+  const attPaid = (punter: PunterProps, id: number | null, index: number) => {
+    api.put(`/api/punter/${id}`, punter).then(
+      (res) => {
+        punters.splice(index, 1, res.data)
+        setPunters(punters)
+      }
+    )
+  }
+
   return (
     <div className={Games.box}>
       <table className={TableStyles.table}>
@@ -20,7 +35,16 @@ export const PuntersTable = ({ punters }: { punters: Array<PunterProps> }) => {
                 <tr key={index}>
                   <td>{punter.name}</td>
                   <td>{punter.guess}</td>
-                  <td><input type="checkbox" checked={punter.paid} /></td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      onChange={() => {
+                        punter.paid = !punter.paid
+                        attPaid(punter, punter.id, index)
+                      }}
+                      checked={punter.paid}
+                    />
+                  </td>
                 </tr>
               )
             }
