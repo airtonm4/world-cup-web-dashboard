a70e531713a1c94999352836d9093c0aaf5e7c99
airtonm4 <airton.atmi@gmail.com>
2022-12-11 11:33:27 -0300

    Using context and saving game

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router"
 import { BrowserRouter } from "react-router-dom"
 import { Game } from "./components/Games"
+import { GameProvider } from "./contexts/GameContext"
 import { Root } from "./pages/Root"
 
 export const Router = () => {
@@ -8,7 +9,7 @@ export const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="" element={<Root />} >
-          <Route path="add-game" element={<Game />} />
+          <Route path="add-game" element={<GameProvider><Game /></GameProvider>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/Games/index.tsx
+++ src/components/Games/index.tsx
@@ -1,17 +1,39 @@
+import { useGameContext } from "../../contexts/GameContext"
+import api from "../../utils/api"
 import Games from "./Games.module.css"
 
 export const Game = () => {
+
+  const { game, setGame } = useGameContext()
+
+  const saveGame = () => {
+    console.log(game)
+    api.post("/api/game", game).then(
+      (res) => {
+        console.log(res)
+      }
+    )
+  }
+
   return (
     <div className={Games.box}>
       <div className={Games.times}>
-        <input type="text" placeholder="Time A"></input>
+        <input
+          type="text"
+          placeholder="Time A"
+          onChange={(e) => setGame((game) => { game.firstTeam = e.target.value })}
+        />
       </div>
 
       <div className={Games.times}>
-        <input type="text" placeholder="Time B"></input>
+        <input
+          type="text"
+          placeholder="Time B"
+          onChange={(e) => setGame((game) => { game.secondTeam = e.target.value })}
+        />
       </div>
 
-      <button className={Games.button} onClick={() => { }}>Salvar</button>
+      <button className={Games.button} onClick={() => saveGame()}>Salvar</button>
     </div>
   )
 }
